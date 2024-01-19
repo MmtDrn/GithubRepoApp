664023dc05197dd36d6fb40318c82430d9ddad09
Mehmet Duran <mmt.durann@gmail.com>
2024-01-19 14:31:36 +0300

    - repo UI is built on homeVC

---
+++ GithubRepoApp/GithubRepoApp/Controller/Home/Cell/HomeVCCell.swift
@@ -0,0 +1,94 @@
+//
+//  HomeVCCell.swift
+//  GithubRepoApp
+//
+//  Created by mehmet duran on 19.01.2024.
+//
+
+import UIKit
+
+class HomeVCCell: UITableViewCell {
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = "GithubRepoApp"
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = "MVVM Design Pattern......blablabla"
+        
+        return label
+    }()
+    
+    private lazy var starCountLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = "⭐️ 3"
+        
+        return label
+    }()
+    
+    private lazy var languageLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = "🔴 Swift"
+        
+        return label
+    }()
+    
+    private lazy var bottomStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [starCountLabel,
+                                                       languageLabel])
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        
+        return stackView
+    }()
+    
+    private lazy var containerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [nameLabel,
+                                                       descriptionLabel,
+                                                       bottomStackView])
+        
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(containerStackView)
+        
+        containerStackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(10)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- GithubRepoApp/GithubRepoApp/Controller/Home/HomeVC.swift
+++ GithubRepoApp/GithubRepoApp/Controller/Home/HomeVC.swift
@@ -14,6 +14,18 @@ class HomeVC: UIViewController {
         return view
     }()
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.register(HomeVCCell.self, forCellReuseIdentifier: String(describing: HomeVCCell.self))
+        tableView.showsVerticalScrollIndicator = false
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -26,10 +38,39 @@ class HomeVC: UIViewController {
     
     private func setupViews() {
         view.addSubview(segmentedButtonsView)
+        view.addSubview(tableView)
+        
         segmentedButtonsView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
             make.leading.trailing.equalToSuperview().inset(10)
             make.height.equalTo(50)
         }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(segmentedButtonsView.snp.bottom).offset(10)
+            make.leading.bottom.trailing.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Tableview methods
+extension HomeVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: HomeVCCell.self), for: indexPath) as! HomeVCCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 }
