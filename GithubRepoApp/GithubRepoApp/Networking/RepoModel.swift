b3e61b2ec56b2b5f0ab56ecdad20b06f38aef2b5
Mehmet Duran <mmt.durann@gmail.com>
2024-01-19 03:54:14 +0300

    - network request router and repo model added

---
+++ GithubRepoApp/GithubRepoApp/Networking/RepoModel.swift
@@ -0,0 +1,49 @@
+//
+//  RepoModel.swift
+//  GithubRepoApp
+//
+//  Created by mehmet duran on 18.01.2024.
+//
+
+import Foundation
+
+struct RepoModel: Codable {
+    var id: Int?
+    var name: String?
+    var `private`: Bool?
+    var owner: Owner?
+    var html_url: String?
+    var description: String?
+    var fork: Bool?
+    var url: String?
+    var forks_url: String?
+    var created_at: String?
+    var updated_at: String?
+    var pushed_at: String?
+    var stargazers_count: Int?
+    var watchers_count: Int?
+    var forks_count: Int?
+}
+
+struct Owner: Codable {
+    var login: String?
+    var id: Int?
+    var node_id: String?
+    var avatar_url: String?
+    var gravatar_id: String?
+    var url: String?
+    var html_url: String?
+    var followers_url: String?
+    var following_url: String?
+    var gists_url: String?
+    var starred_url: String?
+    var subscriptions_url: String?
+    var organizations_url: String?
+    var repos_url: String?
+    var events_url: String?
+    var received_events_url: String?
+    var type: String?
+    var site_admin: Bool?
+}
+
+
---
+++ GithubRepoApp/GithubRepoApp/Networking/Router.swift
@@ -0,0 +1,44 @@
+//
+//  Router.swift
+//  GithubRepoApp
+//
+//  Created by mehmet duran on 18.01.2024.
+//
+
+import Foundation
+import Alamofire
+
+enum Router: EndpointConfiguration {
+    case algorand
+    case perawallet
+    case algorandfoundation
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var path: String {
+        switch self {
+        case .algorand:
+            return "algorand/repos"
+        case .perawallet:
+            return "perawallet/repos"
+        case .algorandfoundation:
+            return "algorandfoundation/repos"
+        }
+    }
+    
+    var queryParameters: QueryStringParameters {
+        return nil
+    }
+    
+    var httpBody: Data? {
+        return nil
+    }
+    
+    var headers: [CustomHTTPHeader]? {
+        return [.deviceType, .deviceId, .localization]
+    }
+    
+    
+}
