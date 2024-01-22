2d8269b24efeaa5f1a5923f60499ea8d64ddb9b0
Mehmet Duran <mmt.durann@gmail.com>
2024-01-22 09:32:08 +0300

    - owner avatar and name added to cell

--- GithubRepoApp/GithubRepoApp/Controller/Home/Cell/HomeVCCell.swift
+++ GithubRepoApp/GithubRepoApp/Controller/Home/Cell/HomeVCCell.swift
@@ -6,19 +6,51 @@
 //
 
 import UIKit
+import Kingfisher
 
 class HomeVCCell: UITableViewCell {
+    
+    private lazy var ownerImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 10
+        
+        return imageView
+    }()
+    
+    private lazy var ownerNameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 26, weight: .bold)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = ""
+        
+        return label
+    }()
     
     private lazy var nameLabel: UILabel = {
         let label = UILabel()
         
-        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
         label.textColor = .black
         label.textAlignment = .left
         label.numberOfLines = 0
         label.text = "GithubRepoApp"
         
         return label
+    }()
+    
+    private lazy var topStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [ownerImageView,
+                                                       ownerNameLabel])
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        
+        return stackView
     }()
     
     private lazy var descriptionLabel: UILabel = {
@@ -68,7 +100,8 @@ class HomeVCCell: UITableViewCell {
     }()
     
     private lazy var containerStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [nameLabel,
+        let stackView = UIStackView(arrangedSubviews: [topStackView,
+                                                       nameLabel,
                                                        descriptionLabel,
                                                        bottomStackView])
         
@@ -86,6 +119,10 @@ class HomeVCCell: UITableViewCell {
         containerStackView.snp.makeConstraints { make in
             make.edges.equalToSuperview().inset(10)
         }
+        
+        ownerImageView.snp.makeConstraints { make in
+            make.height.width.equalTo(30)
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -93,9 +130,13 @@ class HomeVCCell: UITableViewCell {
     }
     
     func configureViews(model: RepoModel) {
+        ownerNameLabel.text = model.owner?.login?.capitalized
         nameLabel.text = model.name
         descriptionLabel.text = model.description
         starCountLabel.text = "⭐️ \(model.stargazers_count ?? 0)"
         languageLabel.text = "🔴 \(model.language ?? "unkown language")"
+        
+        guard let avatarURL = model.owner?.avatar_url else { return }
+        ownerImageView.kf.setImage(with: URL(string: avatarURL))
     }
 }
